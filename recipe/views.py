072d3a7e27f301bcbd1c8c9ea6b9639fef00fa1d
Joe Streifel <josephstreifel@gmail.com>
2022-01-13 18:01:16 -0800

    adds views and serializers

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+class RecipeList(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeChange(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
